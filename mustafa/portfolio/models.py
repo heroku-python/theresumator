2749c5fbf7e5638325b842087d1057369e5543b2
Mustafa Abualsaud <ammsa7@gmail.com>
2015-11-01 22:13:53 -0800

    [#1] Create portfolio app
    as well as init commit. Fixes #1

---
+++ mustafa/portfolio/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class BasicInformation(models.Model):
+    name = models.CharField(max_length=20)
+    short_bio = models.CharField(max_length=200)
+    email = models.EmailField()
+    github = models.URLField()
+    linkedIn = models.URLField()
+    image = models.ImageField()
+
+
+class Projects(models.Model):
+    name = models.CharField(max_length=25)
+    description = models.CharField(max_length=200)
+    link = models.URLField()
+    picture = models.ImageField()
+
+
+class Experience(models.Model):
+    company = models.CharField(max_length=20)
+    role = models.CharField(max_length=20)
+    start_date = models.TimeField()
+    end_date = models.TimeField()
+    description = models.CharField(max_length=200)
+    URL = models.URLField()
+
+
+class Languages(models.Model):
+    name = models.CharField(max_length=10)
+    experience = models.ForeignKey(Experience)
+    projects = models.ForeignKey(Projects)
