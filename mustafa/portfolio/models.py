308510a128e2c0088597007fc3479ffe8b4ec817
Mustafa Abualsaud <ammsa7@gmail.com>
2015-11-02 22:53:22 -0800

    [#4] Limit BasicInformation to a single instance
    Fixes #4

--- mustafa/portfolio/models.py
+++ mustafa/portfolio/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.sites.models import Site
 
 
 class BasicInformation(models.Model):
+    site = models.OneToOneField(Site)
     name = models.CharField(max_length=20)
     short_bio = models.CharField(max_length=200)
     email = models.EmailField()
