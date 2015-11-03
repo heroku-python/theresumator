f57f374c64ca5b395b8c6180a5759caaa36257aa
Mustafa Abualsaud <ammsa7@gmail.com>
2015-11-02 22:45:54 -0800

    [#3] Register models to admin.py
    Update .gitignore and setting.py

--- mustafa/portfolio/admin.py
+++ mustafa/portfolio/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from models import BasicInformation
+from models import Projects
+from models import Experience
+from models import Languages
 
-# Register your models here.
+
+admin.site.register(BasicInformation)
+admin.site.register(Projects)
+admin.site.register(Experience)
+admin.site.register(Languages)
