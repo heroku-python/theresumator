b4a9702d6fb472bbd2a52de5bafe4835f991a3ff
Mustafa Abualsaud <ammsa7@gmail.com>
2015-11-07 17:14:33 -0800

    [#9] Make used_models() to work with all views

---
+++ mustafa/portfolio/utils.py
@@ -0,0 +1,19 @@
+from django.apps import apps
+
+
+def used_models(context_dict):
+        """
+        Checks if models other than whats in `context_dict`
+        have atleast a single instance in db
+        :return context_dict: dict containing already assigned objects,
+                              and other models names set to True if they have
+                              atleast a single instance in db
+        """
+        app_models = apps.get_app_config('portfolio').get_models()
+        for model in app_models:
+            if model._meta.db_table in context_dict:
+                pass
+            else:
+                if model.objects.all():
+                    context_dict[model._meta.db_table] = True
+        return context_dict
--- mustafa/portfolio/views.py
+++ mustafa/portfolio/views.py
@@ -1,40 +1,20 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
-from django.apps import apps
 
 from .models import BasicInformation
 from .models import Project
 from .models import Experience
 from .models import Language
 
+from portfolio import utils
+
 
 def about(request):
     template = loader.get_template('portfolio/index.html')
-
-    # TODO: Change used_models() work with other views
-    # this is just a quick hack to check if other models like projects or education
-    # have been init with objects. This will work for one view though. Chould change to work
-    # with other views
-
-    def used_models():
-        """
-        Checks if models other than `portfolio.models.BasicInformation`
-        have atleast a single instance in db
-        :return context_dict: dict containing `portfolio.models.BasicInformation` object,
-                              and other models names set to True if they have
-                              atleast a single instance in db
-        """
-        context_dict = {}
-        app_models = apps.get_app_config('portfolio').get_models()
-        for model in app_models:
-            if model.__name__ == BasicInformation.__name__:
-                context_dict[model._meta.db_table] = model.objects.get()
-            else:
-                if model.objects.all():
-                    context_dict[model._meta.db_table] = True
-        return context_dict
-
-    context = RequestContext(request, used_models())
+    basic_information = BasicInformation.objects.get()
+    context_dict = {"portfolio_basicinformation": basic_information}
+    context = RequestContext(request,
+                             utils.used_models(context_dict))
     return HttpResponse(template.render(context))
 
 
