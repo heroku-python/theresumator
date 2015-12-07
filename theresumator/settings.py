51ba323a2fdc8d63c54d8cf8d6af6babf5ab518b
Mustafa Abualsaud <AmmsA@users.noreply.github.com>
2015-12-06 22:48:31 -0800

    [#15] Update base_dir to django's default.

--- theresumator/settings.py
+++ theresumator/settings.py
@@ -10,11 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import dj_database_url
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
